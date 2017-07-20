da7a36fe087003303f2c835ecb81bd38937a1df3
SahakyanGit <arman.sahakyan.dev@gmail.com>
2017-07-20 11:06:56 +0400

    Added Index view.Added _Layout.cshtml.Added services.

--- WorkforceManagement/WorkforceManagement.WebUI/Startup.cs
+++ WorkforceManagement/WorkforceManagement.WebUI/Startup.cs
@@ -16,22 +16,31 @@ namespace WorkforceManagement.WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
