0246d41d67d9a71d429b56ca257ce53ddc939057
SahakyanGit <arman.sahakyan.dev@gmail.com>
2017-07-21 17:47:21 +0400

    Added registration page.Updated db.

--- WorkforceManagement/WorkforceManagement.Domain/Entities/Employee.cs
+++ WorkforceManagement/WorkforceManagement.Domain/Entities/Employee.cs
@@ -9,9 +9,9 @@ namespace WorkforceManagement.Domain.Entities
         public int EmployeeId { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public DateTime JoinDate { get; set; }
+        public DateTime Birth { get; set; }
         public string Profession { get; set; }
         //public string Email { get; set; }
-        public string Password { get; set; }
+        //public string Password { get; set; }
     }
 }
