873cb1cdbda2eed85897861d388fc6a9dd1c06d6
SahakyanGit <arman.sahakyan.dev@gmail.com>
2017-07-26 18:49:42 +0400

    Db bug fixed.

--- WorkforceManagement/WorkforceManagement.Domain/Entities/AuthDataModel.cs
+++ WorkforceManagement/WorkforceManagement.Domain/Entities/AuthDataModel.cs
@@ -18,7 +18,5 @@ namespace WorkforceManagement.Domain.Entities
 
         [Required(ErrorMessage ="Please enter your password")]
         public string Password { get; set; }
-
-        public virtual EmployeeModel Employees { get; set; }
     }
 }
