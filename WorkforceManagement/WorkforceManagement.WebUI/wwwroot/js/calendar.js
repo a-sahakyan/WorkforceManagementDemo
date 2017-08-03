29efeac4d204399d59052df89200d1685a10b8c3
SahakyanGit <arman.sahakyan.dev@gmail.com>
2017-08-03 19:24:31 +0400

    Added basic progress bar code.

---
+++ WorkforceManagement/WorkforceManagement.WebUI/wwwroot/js/calendar.js
@@ -0,0 +1,12 @@
+﻿$(".btn-calendar").click(function () {
+    var lable = $(".btn-calendar").text().trim();
+
+    if (lable == "Hide Calendar") {
+        $(".btn-calendar").text("Show Calendar");
+        $(".calender-div").hide();
+    }
+    else {
+        $(".btn-calendar").text("Hide Calendar");
+        $(".calender-div").show();
+    }
+});
