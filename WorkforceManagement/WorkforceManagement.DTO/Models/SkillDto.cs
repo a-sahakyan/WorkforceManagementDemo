ecff5f61ebefc8cc4d1a50782092b3ffc4ddc520
Arman <arman.sahakyan.dev@gmail.com>
2017-08-05 21:23:26 +0400

    Fixed skill naming bug.Added bootstrap.Added ajax.

---
+++ WorkforceManagement/WorkforceManagement.DTO/Models/SkillDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkforceManagement.DTO.Models
+{
+    public class SkillDto
+    {
+        public int SkillDtoId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public string SkillName { get; set; }
+
+        public string SkillKnowledge { get; set; }
+    }
+}
--- WorkforceManagement/WorkforceManagement.WebUI/Controllers/HomeController.cs
+++ WorkforceManagement/WorkforceManagement.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 using WorkforceManagement.BLL.Logic;
 using WorkforceManagement.Domain.Entities;
 using WorkforceManagement.DTO.Models;
@@ -11,7 +14,7 @@ namespace WorkforceManagement.WebUI.Controllers
         IMapLogic<Employee, EmployeeDto> _mapper;
         private IAuthenticationLogic _auth;
 
-        public HomeController(IMapLogic<Employee, EmployeeDto> mapper, IHttpContextAccessor httpContextAccessor,IAuthenticationLogic auth)
+        public HomeController(IMapLogic<Employee, EmployeeDto> mapper, IHttpContextAccessor httpContextAccessor, IAuthenticationLogic auth)
         {
             _mapper = mapper;
             _auth = auth;
@@ -21,11 +24,36 @@ namespace WorkforceManagement.WebUI.Controllers
         public IActionResult Index()
         {
             var employeeDto = _mapper.MapAll();
-            ViewBag.IsAuthenticated = false;
             ViewBag.IsAuthenticated = AuthenticationLogic.IsAuthenticated;
             _auth.SetAuthentication(AuthenticationLogic.IsAuthenticated);
 
             return View(employeeDto);
         }
+
+        public IActionResult Index([FromBody]SkillDto datas)
+        {
+            int a = 24;
+
+            return Json(datas);
+        }
+
+        public IActionResult JsonConfig([FromBody]CountDto data)
+        {
+            string path = @"wwwroot\js\data.json";
+            string json = JsonConvert.SerializeObject(data);
+            using (FileStream fs = new FileStream(path,FileMode.Truncate))
+            using (StreamWriter sr = new StreamWriter(fs))
+            {
+                sr.Write(json);
+            }
+
+            return Json(data);
+        }
+
+    }
+
+    public class CountDto
+    {
+        public string Count { get; set; }
     }
 }
